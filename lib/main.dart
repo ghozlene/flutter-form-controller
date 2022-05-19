ca2343f3a6fa3920cb478647c288f43e670cbda7
Achref <medachrefg@gmail.com>
2022-05-19 15:02:10 +0100

    making some changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,18 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: MyFlutterForm(),
-      ),
+          resizeToAvoidBottomInset: false,
+          body: ListView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  MyFlutterForm(),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
@@ -34,12 +43,12 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
   // variable to enable auto validating of theform
   bool _autoValidate = true;
   // variable to enable toggling between showing and hiding password
-  final distController = TextEditingController();
-  final outPutController = TextEditingController();
-  final dosageController = TextEditingController();
-  final TextEditingController exposureController = TextEditingController();
-  final L = 89;
-  final pi = 3.143;
+  var distController = TextEditingController();
+  var outPutController = TextEditingController();
+  var dosageController = TextEditingController();
+  var exposureController = TextEditingController();
+  var L = 89;
+  var pi = 3.143;
 
   @override
   void dispose() {
@@ -54,21 +63,23 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height,
       padding: EdgeInsets.only(top: 80),
       alignment: Alignment.center,
-      child: Form(
-        key: _formKey,
-        child: Container(
-          padding: EdgeInsets.only(
-            right: 20.0,
-            left: 20.0,
-            top: 40,
-          ),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: ListView(
-            children: [
-              Column(
+      child: Container(
+        child: Form(
+          key: _formKey,
+          child: Container(
+            padding: EdgeInsets.only(
+              right: 20.0,
+              left: 20.0,
+              top: 40,
+            ),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: SingleChildScrollView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   TextFormField(
@@ -82,7 +93,7 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
                     keyboardType: TextInputType.number,
                     validator: validateFloat,
                     inputFormatters: [
-                      FilteringTextInputFormatter.allow(RegExp('[0-9][.]'))
+                      FilteringTextInputFormatter.allow(RegExp('[. 0-9]'))
                     ],
                   ),
                   SizedBox(
@@ -98,7 +109,7 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
                     keyboardType: TextInputType.number,
                     validator: validateFloat,
                     inputFormatters: [
-                      FilteringTextInputFormatter.allow(RegExp('[0-9][.]'))
+                      FilteringTextInputFormatter.allow(RegExp('[. 0-9]'))
                     ],
                   ),
                   SizedBox(
@@ -115,7 +126,7 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
                     keyboardType: TextInputType.number,
                     validator: validateFloat,
                     inputFormatters: [
-                      FilteringTextInputFormatter.allow(RegExp('[0-9][.]'))
+                      FilteringTextInputFormatter.allow(RegExp('[. 0-9]'))
                     ],
                   ),
                   SizedBox(
@@ -132,7 +143,7 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
                     keyboardType: TextInputType.number,
                     validator: validateFloat,
                     inputFormatters: [
-                      FilteringTextInputFormatter.allow(RegExp('[0-9][.]'))
+                      FilteringTextInputFormatter.allow(RegExp('[. 0-9]'))
                     ],
                   ),
                   SizedBox(
@@ -143,19 +154,25 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
                     child: RaisedButton(
                       color: Colors.blue,
                       onPressed: () {
-                        if (exposureController.text == "") {
-                          exposureController.text = ((2 *
-                                      pi *
-                                      L *
-                                      double.parse(distController.text) *
-                                      double.parse(dosageController.text)) /
-                                  double.parse(outPutController.text))
-                              .toString();
-                          if (_formKey.currentState.validate()) {
-                            Scaffold.of(context).showSnackBar(SnackBar(
-                              content: Text('Form Validated, No errors'),
-                            ));
-                          }
+                        if (exposureController.text != "" ||
+                            exposureController.text == '') {
+                          setState(() {
+                            distController = distController;
+                            dosageController = dosageController;
+                            outPutController = outPutController;
+                            exposureController.text = ((2 *
+                                        pi *
+                                        L *
+                                        double.parse(distController.text) *
+                                        double.parse(dosageController.text)) /
+                                    double.parse(outPutController.text))
+                                .toString();
+                          });
+                        }
+                        if (_formKey.currentState.validate()) {
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Form Validated, No errors'),
+                          ));
                         }
                       },
                       textColor: Colors.white,
@@ -171,7 +188,7 @@ class _MyFlutterFormState extends State<MyFlutterForm> {
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
